75d7af3d6c1694563f18b302eb3bc36d623b9c90
yu-v-m123 <nagase.yuki0123@gmail.com>
2022-04-26 00:18:01 +0900

    [feature]ヘッダーとフッター実装

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-footer color="teal lighten-3">
+      <!-- <v-row> -->
+        <v-col class="text-center">©︎ 2022 NyanLeave</v-col>
+      <!-- </v-row> -->
+    </v-footer>
+</template>
