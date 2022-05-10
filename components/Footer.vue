6f6d390ca73b258e5a2efcfc3175d2d96d1f9f2c
yu-v-m123 <nagase.yuki0123@gmail.com>
2022-05-11 01:21:29 +0900

    [add]Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,5 @@
 <template>
     <v-footer color="teal lighten-3">
-      <!-- <v-row> -->
         <v-col class="text-center">©︎ 2022 NyanLeave</v-col>
-      <!-- </v-row> -->
     </v-footer>
 </template>
